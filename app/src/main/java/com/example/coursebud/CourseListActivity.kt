d4226aba2fd03c31c5af18b911dc407fd9f41dcc
jbt89 <jbt89@cornell.edu>
2021-11-21 23:43:48 -0500

    Added RecyclerView to CourseListActivity.kt

--- app/src/main/java/com/example/coursebud/CourseListActivity.kt
+++ app/src/main/java/com/example/coursebud/CourseListActivity.kt
@@ -2,10 +2,30 @@ package com.example.coursebud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CourseListActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var mAdapter : RecyclerView.Adapter<*>
+    private lateinit var layoutManager : RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_list)
+
+        recyclerView = findViewById<RecyclerView>(R.id.list)
+
+        recyclerView.setHasFixedSize(true)
+        // use a linear layout manager
+        layoutManager = LinearLayoutManager(this@CourseListActivity)
+        recyclerView.layoutManager = layoutManager
+        // create dataset, format should match what you specified 
+        // in the CourseAdapter object
+        var courses = mutableListOf<String>()
+
+        mAdapter = CourseAdapter(courses)
+        recyclerView.adapter = mAdapter
     }
 }
