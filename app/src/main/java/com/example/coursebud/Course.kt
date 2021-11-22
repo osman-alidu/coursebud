1a3a55e7d5111faaf255a7b6917efaf263d01f67
Phyllis Ju <kj234@cornell.edu>
2021-11-21 22:09:37 -0500

    Added a data class of the course cell

---
+++ app/src/main/java/com/example/coursebud/Course.kt
@@ -0,0 +1,3 @@
+package com.example.coursebud
+
+data class Course(val index:Int, val level:String, val course_number:String, val stars:String, val review_number:Int)
---
+++ app/src/main/java/com/example/coursebud/CourseAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.coursebud
+
+class CourseAdapter {
+}
