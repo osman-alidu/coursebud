ad9dc1ae2114a3c245a2d2c216a7fc7f8e494194
Phyllis Ju <kj234@cornell.edu>
2021-12-01 22:14:14 -0500

    Added a homepage layout

--- app/src/main/java/com/example/coursebud/MainActivity.kt
+++ app/src/main/java/com/example/coursebud/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.coursebud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appTitle : TextView
+    private lateinit var signUpBtn : Button
+    private lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appTitle = findViewById(R.id.appTitle)
+        signUpBtn = findViewById(R.id.signUpBtn)
+        loginBtn = findViewById(R.id.loginBtn)
     }
 }
