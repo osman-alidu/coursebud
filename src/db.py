c60f97c867f0ed4ba0c77f15a5cbd111cf921196
osmanalidu <alidunapare00@gmail.com>
2021-12-02 09:48:26 -0500

    bcrypt uncommented

--- src/db.py
+++ src/db.py
@@ -100,8 +100,8 @@ class User(db.Model):
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
         self.email = kwargs.get("email")
-        self.password_digest = bcrypt.hashpw(kwargs.get(
-            "password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        # self.password_digest = bcrypt.hashpw(kwargs.get(
+        #     "password").encode("utf8"), bcrypt.gensalt(rounds=13))
         self.renew_session()
 
     def serialize(self):
@@ -117,22 +117,22 @@ class User(db.Model):
             "name": self.name,
         }
 
-    # Used to randomly generate session/update tokens
-    def _urlsafe_base_64(self):
-        return hashlib.sha1(os.urandom(64)).hexdigest()
+    # # Used to randomly generate session/update tokens
+    # def _urlsafe_base_64(self):
+    #     return hashlib.sha1(os.urandom(64)).hexdigest()
 
-    # Generates new tokens, and resets expiration time
-    def renew_session(self):
-        self.session_token = self._urlsafe_base_64()
-        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
-        self.update_token = self._urlsafe_base_64()
+    # # Generates new tokens, and resets expiration time
+    # def renew_session(self):
+    #     self.session_token = self._urlsafe_base_64()
+    #     self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+    #     self.update_token = self._urlsafe_base_64()
 
-    def verify_password(self, password):
-        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+    # def verify_password(self, password):
+    #     return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
 
-    # Checks if session token is valid and hasn't expired
-    def verify_session_token(self, session_token):
-        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+    # # Checks if session token is valid and hasn't expired
+    # def verify_session_token(self, session_token):
+    #     return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
 
-    def verify_update_token(self, update_token):
-        return update_token == self.update_token
+    # def verify_update_token(self, update_token):
+    #     return update_token == self.update_token
